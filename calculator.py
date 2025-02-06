947b95a38a1a3bfb49ca43ea36b413ac0222fda0
sudhanshu shekhar <107687045+sudhanshushekr@users.noreply.github.com>
2025-02-07 04:00:47 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,48 +1,57 @@
 # Creating Calculator 
 
-
 def calculator():
-    
-    # methods for calculation function perations 
+    # methods for calculation operations 
 
     def add(n1, n2):
+        # function to add two numbers
         return n1 + n2
 
     def subtract(n1, n2):
+        # function to subtract second number from first number
         return n1 - n2
 
     def multiply(n1, n2):
+        # function to multiply two numbers
         return n1 * n2
 
     def divide(n1, n2):
+        # function to divide first number by second number
         return n1 / n2
 
+    # dictionary to store operations and their corresponding functions
     operations = {
         "+": add,
         "-": subtract,
         "*": multiply,
         "/": divide
-
     }
+
+    # prompt user to input the first number
     num1 = float(input("What is the first number?: "))
 
-    # user should be able to veiw the operator symbol
-
+    # display the operator symbols to the user
     for symbol in operations:
         print(symbol)
+    
     should_continue = True
 
-    #  user sellect the operator symbol and operation starts
-
+    # user selects the operator symbol and the operation starts
     while should_continue:
         operation_symbol = input("Pick an operation: ")
+        # prompt user to input the next number
         num2 = float(input("What is the next number?: "))
+        # retrieve the calculation function based on the selected operator
         calculation_function = operations[operation_symbol]
+        # perform the calculation
         answer = calculation_function(num1, num2)
+        # display the result of the calculation
         print(f"{num1} {operation_symbol} {num2} = {answer}")
 
+        # prompt user to continue with the current result or start a new calculation
         if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
             num1 = answer
         else:
             should_continue = False
+            # restart the calculator for a new calculation
             calculator()
